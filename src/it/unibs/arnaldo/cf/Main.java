2cfd49d3eb02271abf7b5cd766b4701db5e62278
ImAlbi57 <48138568+ImAlbi57@users.noreply.github.com>
2021-04-28 18:33:17 +0200

    Fixed psvm

--- src/it/unibs/arnaldo/cf/Main.java
+++ src/it/unibs/arnaldo/cf/Main.java
@@ -1,7 +1,7 @@
 package it.unibs.arnaldo.cf;
 
 public class Main {
-	public static void main(String s[]) {
+	public static void main(String args[]) {
 		System.out.println("Se non va mi uccido");
 	}
 }
